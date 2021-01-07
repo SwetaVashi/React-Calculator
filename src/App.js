18501e6bc7f496e21691fcb75678e8d39d9c331a
SwetaVashi <73024611+SwetaVashi@users.noreply.github.com>
2021-01-07 15:06:22 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Controller from './CalApp/Controller'
+
+
+
+
+const App=()=>{
+
+
+  return (
+          <div className="App">
+              <Controller />
+          
+          </div>
+  )
+}
+
+export default App;
